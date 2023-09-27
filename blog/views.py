3ff5235cbdcdc7032c3657f0fc8352f694adcce1
Sandesh Nandagawe <sandagawe@gmail.com>
2023-09-27 14:42:51 +0530

    Blog page to nevigate to individual blogs fromurl

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
+app_name = 'blog'
+
 def all_blogs(request):
-    blog = Blog.objects.all()
+    blog = Blog.objects.all() [:3]
     return render(request, 'blog/all_blogs.html', {'blogs':blog})
+
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk = blog_id)
+    return render(request, 'blog/detail.html', {'blog':blog})
